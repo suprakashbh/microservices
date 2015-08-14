1fc2db7215123ff68ac80f99c0ef9d566aaf2c0e
suprakash.bh@gmail.com <suprakash.bh@gmail.com>
2015-08-14 12:16:07 +0530

    updated

--- WeatherMicroService/src/com/infy/microservice/rest/WeatherForecastController.java
+++ WeatherMicroService/src/com/infy/microservice/rest/WeatherForecastController.java
@@ -1,5 +1,12 @@
 package com.infy.microservice.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,19 +19,28 @@ import com.infy.model.Weather;
 @RestController
 @EnableAutoConfiguration
 public class WeatherForecastController {
+	
+	@Value("${spring.application.stub}")
+	private boolean stub;
 
 	@RequestMapping("/weather/{city}")
 	@ResponseBody
 	Weather getWeather(@PathVariable String city) {
+		
+		System.out.println("stub ---->"+stub);
+		if(!stub){
+			RestTemplate restTemplate = new RestTemplate();
 
-		RestTemplate restTemplate = new RestTemplate();
+			System.out.println(" restTemplate :::" + restTemplate);
+			Weather weather = restTemplate.getForObject(
+					"http://api.openweathermap.org/data/2.5/weather?q=" + city
+							+ "&units=Metric", Weather.class);
 
-		System.out.println(" restTemplate :::" + restTemplate);
-		Weather weather = restTemplate.getForObject(
-				"http://api.openweathermap.org/data/2.5/weather?q=" + city
-						+ "&units=Metric", Weather.class);
-
-		return weather;
+			return weather;
+		}else {
+			return getStubWeatherData(city);
+		}
+		
 	}
 
 	@RequestMapping("/weather")
@@ -32,6 +48,9 @@ public class WeatherForecastController {
 	Weather getWeather() {
 		// default to Pune
 		String city = "Pune";
+		if(stub){
+			return getStubWeatherData(city);
+		}
 
 		RestTemplate restTemplate = new RestTemplate();
 
@@ -42,5 +61,27 @@ public class WeatherForecastController {
 
 		return weather;
 	}
+	
+	private Weather getStubWeatherData(String city){
+		String mockFile = "/data/weather_"+city.toLowerCase()+".json";
+		ObjectMapper objectMapper = new ObjectMapper();
+		Weather weather = null;
+		try {
+			InputStream stream = getClass().getResourceAsStream(mockFile);
+			weather = objectMapper.readValue(stream, Weather.class);
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return weather;
+	}
+	
 
 }
